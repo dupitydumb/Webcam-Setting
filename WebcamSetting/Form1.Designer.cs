547519b54907773446f4a8b03686e0e093c69414
AvKov <37872714+dupitydumb@users.noreply.github.com>
2023-12-31 02:48:06 +0700

    Add project files.

---
+++ WebcamSetting/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WebcamSetting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 22);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.comboBox2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(241, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(547, 426);
+            this.panel2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(541, 375);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(9, 9);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(779, 440);
+            this.panel3.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(3, 386);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(200, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(209, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Setting";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 461);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.MaximumSize = new System.Drawing.Size(816, 500);
+            this.MinimumSize = new System.Drawing.Size(816, 500);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Webcam Setting";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WebcamSetting/Form1.cs
@@ -0,0 +1,152 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WebcamSetting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.BackColor = Color.FromArgb(0, Color.Black);
+            comboBox1.SelectionChangeCommitted += new EventHandler(comboBox1_SelectionChangeCommitted);
+            comboBox2.SelectionChangeCommitted += new EventHandler(comboBox2_SelectionChangeCommitted);
+            button1.Click += new EventHandler(button1_Click);
+
+
+            GetCamera();
+
+        }
+
+        public event EventHandler SelectionChangeCommitted;
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+
+        string videoSourceName;
+
+        private void GetCamera()
+        {
+            comboBox1.Items.Add("Select Camera");
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+            {
+                comboBox1.Items.Add(filterInfo.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+            videoSourceName = comboBox1.SelectedItem.ToString();
+
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[comboBox1.SelectedIndex - 1].MonikerString);
+            videoSourceName = comboBox1.SelectedItem.ToString();
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+
+            foreach (var resolution in videoCaptureDevice.VideoCapabilities)
+            {
+                comboBox2.Items.Add(resolution.FrameSize.Width + "x" + resolution.FrameSize.Height);
+            }
+
+
+
+     
+        }
+
+        private void comboBox2_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            videoCaptureDevice.Stop();
+            videoCaptureDevice.VideoResolution = videoCaptureDevice.VideoCapabilities[comboBox2.SelectedIndex];
+            videoCaptureDevice.Start();
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        //On form close
+
+        //Run Bat file to setting webcam
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Setting();
+        }
+
+
+        private void Setting()
+        {
+            try
+            {
+                if (videoCaptureDevice.IsRunning != null && videoCaptureDevice.IsRunning)
+                {
+                    //check if setting.bat is exist at Documents/WebcamSetting/setting.bat
+                    if (System.IO.File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\WebcamSetting\\setting.bat"))
+                    {
+
+                        ExcecuteBat();
+                    }
+                    else
+                    {
+                        //Show yes no dialog
+                        MessageBox.Show("setting.bat not found, do you want to create it?", "Warning", MessageBoxButtons.YesNo);
+                        //if not exist, create it
+                        Writebat();
+                        //Run bat file
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select camera", "Warning", MessageBoxButtons.OK);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please Select Camera", "Error", MessageBoxButtons.OK);
+            }
+            
+            
+        }
+
+
+        private void Writebat()
+        {
+            //Create new path file at Documents/WebcamSetting/setting.bat
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\WebcamSetting\\setting.bat";
+
+            //Create new folder at Documents/WebcamSetting
+            System.IO.Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\WebcamSetting");
+
+            //Copy ffmpeg.exe to Documents/WebcamSetting/ffmpeg.exe
+            System.IO.File.Copy(AppDomain.CurrentDomain.BaseDirectory + "ffmpeg.exe", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\WebcamSetting\\ffmpeg.exe", true);
+            //Write bat file
+            string[] lines = { "chcp 65001 > nul", "ffmpeg -f dshow -show_video_device_dialog true -i video=" + '"' + videoSourceName + '"' };
+            System.IO.File.WriteAllLines(path, lines);
+            MessageBox.Show("Exported to " + path);
+
+        }
+
+        private void ExcecuteBat()
+        {
+            try
+            {
+                //get root path
+                string path = AppDomain.CurrentDomain.BaseDirectory + "setting.bat";
+                //Open bat file
+                System.Diagnostics.Process.Start(path);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("File not found", "Error", MessageBoxButtons.OK);
+            }
+            
+            
+        }
+    }
+}
